4a66ca69aeeaef1808fd7bfc8d87850d8860d894
Dragonbestgg1 <dragonbestgg1@gmail.com>
2024-06-10 16:00:02 +0300

    .

---
+++ src/home/home.js
@@ -0,0 +1,38 @@
+import React from "react";
+import style from "../styles/home.module.css";
+
+function Home() {
+    return (
+        <div className={`${style.main}`}>
+            <div className={`${style.header}`}>
+                
+            </div>
+            <div className={`${style.who_I_am}`}>
+
+            </div>
+            <div className={`${style.about_me}`}>
+
+            </div>
+            <div className={`${style.my_skills}`}>
+
+            </div>
+            <div className={`${style.education}`}>
+
+            </div>
+            <div className={`${style.project_list}`}>
+
+            </div>
+            <div className={`${style.work_exp}`}>
+
+            </div>
+            <div className={`${style.reviews}`}>
+
+            </div>
+            <div className={`${style.contact_me}`}>
+
+            </div>
+        </div>
+    )
+}
+
+export default Home;
